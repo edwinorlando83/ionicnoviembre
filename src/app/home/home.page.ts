2ab7f79e9aa14f4ba4dd954a5b910b9e1c9ed593
Orlando <edwin_orlando83@hotmail.com>
2020-11-25 20:11:46 -0500

    archivos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nombre;
+  sueldo= 400.89;
+  imagen_logo1 = 'assets/i1.png';
+  imagen_logo2= 'assets/i2.png';
 
+  cedula;
+  mensaje='';
+  verMensaje=false;
+  lista=[{'codigo':1,'nombre':'orlando' } 
+  , {'codigo':2,'nombre':'edwin' } ,
+  {'codigo':2,'nombre':'edwin 1' } ,
+  {'codigo':2,'nombre':'edwin 2' } ,
+  {'codigo':2,'nombre':'edwin 3' } ,
+  {'codigo':2,'nombre':'edwin 4' } ,
+  {'codigo':2,'nombre':'edwin 5' } ,
+  {'codigo':2,'nombre':'edwin 6' } ,
+  {'codigo':2,'nombre':'edwin' } ,
+  {'codigo':2,'nombre':'edwin' } ,
+  {'codigo':2,'nombre':'edwin' } ,
+  {'codigo':2,'nombre':'edwin' } ,
+  {'codigo':3,'nombre':'adriana' }   ];
+
+  constructor() {
+    this.nombre ='Orlando';
+    this.sueldo += 10;
+  }
+  click(){
+    if(  this.cedula.length != 10 ){
+       this.verMensaje=true;     
+    }   
+  }
+  modificar(valor){
+    alert(valor);
+  }
+
+ 
 }
